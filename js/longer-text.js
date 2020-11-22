1cb8abcbeb542996c9c9a5efbe4c8c073ee33d82
Andriy Mosol <40470899+Mosandr@users.noreply.github.com>
2020-11-22 21:19:34 +0200

    Add content to slider cards

--- js/longer-text.js
+++ js/longer-text.js
@@ -1,14 +1,10 @@
-(() => {
-  const refs = {
-    makeLong: document.querySelector("[data-makeLong]"),
-    makeShort: document.querySelector("[data-makeShort]"),
-    description: document.querySelector("[data-description]"),
-  };
 
-  refs.makeLong.addEventListener("click", toggleModal);
-  refs.makeShort.addEventListener("click", toggleModal);
+$(function () {
+  $('.long-switch-btn').click(function (e) {
+    $('.testimonial__description').toggleClass('testimonial__description--long');
+  });
 
-  function toggleModal() {
-    refs.description.classList.toggle("testimonial__description--long");
-  }
-})();
+  $('.short-switch-btn').click(function (e) {
+    $('.testimonial__description').toggleClass('testimonial__description--long');
+  });
+});
--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,11 @@
 $(document).on('ready', function () {
       
-    $(".testimonial__slider").slick({
-      infinite: true,
+  $(".testimonial__slider").slick({
+      dots: true,
+      infinite: true,      
+      slidesToShow: 1,
       nextArrow: '.testimonials__next-btn',
-      prevArrow: '.testimonials__prev-btn',
-    });  
-    
+      prevArrow: '.testimonials__prev-btn',         
+    }); 
+  
 });    
